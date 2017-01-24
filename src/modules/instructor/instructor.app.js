dd091656277493ad6843a068b1b8408ea8706af1
anurag <anurag98350sharma@gmail.com>
2017-01-24 14:54:04 +0530

    initial work

---
+++ src/modules/instructor/instructor.app.js
@@ -0,0 +1,3 @@
+(function(){
+	angular.module("instructor.app",[])
+})()
---
+++ src/modules/shared/angular-form-validation/angular-form-validation.js
@@ -0,0 +1,29 @@
+angular.module("angular-form-validation",[])
+
+.directive("onlyText",function(){
+	return {
+		restrict : E,
+		template : ""
+	}
+})
+
+.directive("text",function(){
+	return {
+		restrict : E,
+		template : ""
+	}
+})
+
+.directive("number",function(){
+	return {
+		restrict : E,
+		template : ""
+	}
+})
+
+.directive("email", function() {
+	return {
+		restrict : E,
+		template : ""
+	}
+});
